307ec9c27b665acd026b33688b5fce3477fc5792
mclaramarinho <marinho.claramb@gmail.com>
2025-01-27 21:58:56 -0300

    changed template button name

--- lib/components/TemplateSBButton/index.tsx
+++ lib/components/TemplateSBButton/index.tsx
@@ -1,5 +1,5 @@
 import React  from 'react'
 
-export function Button(){
+export function TemplateSBButton(){
     return <button>aaaaaa</button>
 }
--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
-import {Button} from '@/components/Button/index.tsx';
+import {TemplateSBButton as Button} from '@/components/TemplateSBButton';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
