fb6f7e629a0cbdfc452123bb37b9fc997842d440
mclaramarinho <marinho.claramb@gmail.com>
2025-01-28 23:27:45 -0300

    project setup + storybook added + template button created

---
+++ .storybook/main.ts
@@ -0,0 +1,26 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  viteFinal: (config) => {
+    return{
+      ...config,
+      css: {
+        modules: {
+          localsConvention: "camelCase"
+        }
+      }
+    }
+  }
+};
+export default config;
