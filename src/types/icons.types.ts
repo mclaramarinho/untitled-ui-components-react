63e2980a307c66c0890097ad56378b475b6580cd
mclaramarinho <marinho.claramb@gmail.com>
2025-02-06 22:23:28 -0300

    code documentation

--- src/types/icons.types.ts
+++ src/types/icons.types.ts
@@ -10,29 +10,56 @@ export default interface IconBaseType extends ImageBaseType{
     size?: ImageSizeType;
 }
 
-
-// TODO - check if needed
+/**
+ * Used to set the icon to default sizes. Is passed as prop to components.
+ * @class
+ * @example size={FixedIconSize.SM}
+ */
 export class FixedIconSize {
+    /**
+     * Extra-small size (12x12)
+     * @static
+     */
     static get XS () : ImageSizeType {
         return { width: 12, height: 12 };
     }
 
+     /**
+     * Small size (16x16)
+     * @static
+     */
     static get SM () : ImageSizeType {
         return { width: 16, height: 16 };
     }
 
+     /**
+     * Medium size (20x20)
+     * @static
+     */
     static get MD () : ImageSizeType {
         return { width: 20, height: 20 };
     }
 
+     /**
+     * Large size (24x24)
+     * @static
+     */
     static get LG () : ImageSizeType {
         return { width: 24, height: 24 };
     }
     
+     /**
+     * Extra-large size (32x32)
+     * @static
+     */
     static get XL () : ImageSizeType {
         return { width: 32, height: 32 };
     }
     
+     /**
+     * Double-extra-large size (48x48)
+     * @static
+     */
     static get XXL () : ImageSizeType {
         return { width: 48, height: 48 };
     }
