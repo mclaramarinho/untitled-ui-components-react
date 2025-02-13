655fb6a0e3d3a2d4ea85918b62dc23bbdb86bfec
mclaramarinho <marinho.claramb@gmail.com>
2025-02-12 21:02:26 -0300

    added missing colors

--- src/types/colors.types.ts
+++ src/types/colors.types.ts
@@ -12,7 +12,7 @@ interface UntitledColorInfo {
 
 interface UntitledColorShadesList {
     [index:number]: UntitledColorShadeInfo;
-    [index:string]: UntitledColorShadeInfo;
+    [index: string]: UntitledColorShadeInfo;
     25: UntitledColorShadeInfo;
     50: UntitledColorShadeInfo;
     100: UntitledColorShadeInfo;
@@ -30,7 +30,6 @@ interface UntitledColorShadeInfo {
     hex: string;
 }
 
-// TODO - Missing secondary colors
 export class UntitledColorsList {
     static get AllColors () {
         return {
@@ -38,7 +37,15 @@ export class UntitledColorsList {
             "brand": this.PrimaryBrand,
             "error": this.PrimaryError,
             "warning": this.PrimaryWarning,
-            "success": this.PrimarySuccess
+            "success": this.PrimarySuccess,
+            "blue-gray": this.SecondaryBlueGray,
+            "blue-light": this.SecondaryBlueLight,
+            "blue": this.SecondaryBlue,
+            "indigo": this.SecondaryIndigo,
+            "purple": this.SecondaryPurple,
+            "pink": this.SecondaryPink,
+            "rose": this.SecondaryRose,
+            "orange": this.SecondaryOrange
         } as UntitledColorsMap;
     }
     static get PrimaryGray () {
@@ -246,6 +253,342 @@ export class UntitledColorsList {
                 } as UntitledColorShadeInfo,
                 900: {
                     hex: "#054F31"
+                } as UntitledColorShadeInfo,
+            } as UntitledColorShadesList
+        } as UntitledColorInfo;
+    }
+    
+    static get SecondaryBlueGray () {
+        return {
+            name: "SecondaryBlueGray",
+            cssVariablePrefix: "secondary-blue-gray",
+            shades: {
+                25: {
+                    hex: "#FCFCFD"
+                } as UntitledColorShadeInfo,
+                50: {
+                    hex: "#F8F9FC"
+                } as UntitledColorShadeInfo,
+                100: {
+                    hex: "#EAECF5"
+                } as UntitledColorShadeInfo,
+                200: {
+                    hex: "#D5D9EB"
+                } as UntitledColorShadeInfo,
+                300: {
+                    hex: "#AFB5D9"
+                } as UntitledColorShadeInfo,
+                400: {
+                    hex: "#717BBC"
+                } as UntitledColorShadeInfo,
+                500: {
+                    hex: "#4E5BA6"
+                } as UntitledColorShadeInfo,
+                600: {
+                    hex: "#3E4784"
+                } as UntitledColorShadeInfo,
+                700: {
+                    hex: "#363F72"
+                } as UntitledColorShadeInfo,
+                800: {
+                    hex: "#293056"
+                } as UntitledColorShadeInfo,
+                900: {
+                    hex: "#101323"
+                } as UntitledColorShadeInfo,
+            } as UntitledColorShadesList
+        } as UntitledColorInfo;
+    }
+
+    static get SecondaryBlueLight () {
+        return {
+            name: "SecondaryBlueLight",
+            cssVariablePrefix: "secondary-blue-light",
+            shades: {
+                25: {
+                    hex: "#F5FBFF"
+                } as UntitledColorShadeInfo,
+                50: {
+                    hex: "#F0F9FF"
+                } as UntitledColorShadeInfo,
+                100: {
+                    hex: "#E0F2FE"
+                } as UntitledColorShadeInfo,
+                200: {
+                    hex: "#B9E6FE"
+                } as UntitledColorShadeInfo,
+                300: {
+                    hex: "#7CD4FD"
+                } as UntitledColorShadeInfo,
+                400: {
+                    hex: "#36BFFA"
+                } as UntitledColorShadeInfo,
+                500: {
+                    hex: "#0BA5EC"
+                } as UntitledColorShadeInfo,
+                600: {
+                    hex: "#0086C9"
+                } as UntitledColorShadeInfo,
+                700: {
+                    hex: "#026AA2"
+                } as UntitledColorShadeInfo,
+                800: {
+                    hex: "#065986"
+                } as UntitledColorShadeInfo,
+                900: {
+                    hex: "#0B4A6F"
+                } as UntitledColorShadeInfo,
+            } as UntitledColorShadesList
+        } as UntitledColorInfo;
+    }
+
+    static get SecondaryBlue () {
+        return {
+            name: "SecondaryBlue",
+            cssVariablePrefix: "secondary-blue",
+            shades: {
+                25: {
+                    hex: "#F5FAFF"
+                } as UntitledColorShadeInfo,
+                50: {
+                    hex: "#EFF8FF"
+                } as UntitledColorShadeInfo,
+                100: {
+                    hex: "#D1E9FF"
+                } as UntitledColorShadeInfo,
+                200: {
+                    hex: "#B2DDFF"
+                } as UntitledColorShadeInfo,
+                300: {
+                    hex: "#84CAFF"
+                } as UntitledColorShadeInfo,
+                400: {
+                    hex: "#53B1FD"
+                } as UntitledColorShadeInfo,
+                500: {
+                    hex: "#2E90FA"
+                } as UntitledColorShadeInfo,
+                600: {
+                    hex: "#1570EF"
+                } as UntitledColorShadeInfo,
+                700: {
+                    hex: "#175CD3"
+                } as UntitledColorShadeInfo,
+                800: {
+                    hex: "#1849A9"
+                } as UntitledColorShadeInfo,
+                900: {
+                    hex: "#194185"
+                } as UntitledColorShadeInfo,
+            } as UntitledColorShadesList
+        } as UntitledColorInfo;
+    }
+    
+    static get SecondaryIndigo () {
+        return {
+            name: "SecondaryIndigo",
+            cssVariablePrefix: "secondary-indigo",
+            shades: {
+                25: {
+                    hex: "#F5F8FF"
+                } as UntitledColorShadeInfo,
+                50: {
+                    hex: "#EEF4FF"
+                } as UntitledColorShadeInfo,
+                100: {
+                    hex: "#E0EAFF"
+                } as UntitledColorShadeInfo,
+                200: {
+                    hex: "#C7D7FE"
+                } as UntitledColorShadeInfo,
+                300: {
+                    hex: "#A4BCFD"
+                } as UntitledColorShadeInfo,
+                400: {
+                    hex: "#8098F9"
+                } as UntitledColorShadeInfo,
+                500: {
+                    hex: "#6172F3"
+                } as UntitledColorShadeInfo,
+                600: {
+                    hex: "#444CE7"
+                } as UntitledColorShadeInfo,
+                700: {
+                    hex: "#3538CD"
+                } as UntitledColorShadeInfo,
+                800: {
+                    hex: "#2D31A6"
+                } as UntitledColorShadeInfo,
+                900: {
+                    hex: "#2D3282"
+                } as UntitledColorShadeInfo,
+            } as UntitledColorShadesList
+        } as UntitledColorInfo;
+    }
+
+    static get SecondaryPurple () {
+        return {
+            name: "SecondaryPurple",
+            cssVariablePrefix: "secondary-purple",
+            shades: {
+                25: {
+                    hex: "#FAFAFF"
+                } as UntitledColorShadeInfo,
+                50: {
+                    hex: "#F4F3FF"
+                } as UntitledColorShadeInfo,
+                100: {
+                    hex: "#EBE9FE"
+                } as UntitledColorShadeInfo,
+                200: {
+                    hex: "#D9D6FE"
+                } as UntitledColorShadeInfo,
+                300: {
+                    hex: "#BDB4FE"
+                } as UntitledColorShadeInfo,
+                400: {
+                    hex: "#9B8AFB"
+                } as UntitledColorShadeInfo,
+                500: {
+                    hex: "#7A5AF8"
+                } as UntitledColorShadeInfo,
+                600: {
+                    hex: "#6938EF"
+                } as UntitledColorShadeInfo,
+                700: {
+                    hex: "#5925DC"
+                } as UntitledColorShadeInfo,
+                800: {
+                    hex: "#4A1FB8"
+                } as UntitledColorShadeInfo,
+                900: {
+                    hex: "#3E1C96"
+                } as UntitledColorShadeInfo,
+            } as UntitledColorShadesList
+        } as UntitledColorInfo;
+    }
+
+    static get SecondaryPink () {
+        return {
+            name: "SecondaryPink",
+            cssVariablePrefix: "secondary-pink",
+            shades: {
+                25: {
+                    hex: "#FEF6FB"
+                } as UntitledColorShadeInfo,
+                50: {
+                    hex: "#FDF2FA"
+                } as UntitledColorShadeInfo,
+                100: {
+                    hex: "#FCE7F6"
+                } as UntitledColorShadeInfo,
+                200: {
+                    hex: "#FCCEEE"
+                } as UntitledColorShadeInfo,
+                300: {
+                    hex: "#FAA7E0"
+                } as UntitledColorShadeInfo,
+                400: {
+                    hex: "#F670C7"
+                } as UntitledColorShadeInfo,
+                500: {
+                    hex: "#EE46BC"
+                } as UntitledColorShadeInfo,
+                600: {
+                    hex: "#DD2590"
+                } as UntitledColorShadeInfo,
+                700: {
+                    hex: "#C11574"
+                } as UntitledColorShadeInfo,
+                800: {
+                    hex: "#9E165F"
+                } as UntitledColorShadeInfo,
+                900: {
+                    hex: "#851651"
+                } as UntitledColorShadeInfo,
+            } as UntitledColorShadesList
+        } as UntitledColorInfo;
+    }
+
+    static get SecondaryRose () {
+        return {
+            name: "SecondaryRose",
+            cssVariablePrefix: "secondary-rose",
+            shades: {
+                25: {
+                    hex: "#FFF5F6"
+                } as UntitledColorShadeInfo,
+                50: {
+                    hex: "#FFF1F3"
+                } as UntitledColorShadeInfo,
+                100: {
+                    hex: "#FFE4E8"
+                } as UntitledColorShadeInfo,
+                200: {
+                    hex: "#FECDD6"
+                } as UntitledColorShadeInfo,
+                300: {
+                    hex: "#FEA3B4"
+                } as UntitledColorShadeInfo,
+                400: {
+                    hex: "#FD6F8E"
+                } as UntitledColorShadeInfo,
+                500: {
+                    hex: "#F63D68"
+                } as UntitledColorShadeInfo,
+                600: {
+                    hex: "#E31B54"
+                } as UntitledColorShadeInfo,
+                700: {
+                    hex: "#C01048"
+                } as UntitledColorShadeInfo,
+                800: {
+                    hex: "#A11043"
+                } as UntitledColorShadeInfo,
+                900: {
+                    hex: "#89123E"
+                } as UntitledColorShadeInfo,
+            } as UntitledColorShadesList
+        } as UntitledColorInfo;
+    }
+
+    static get SecondaryOrange () {
+        return {
+            name: "SecondaryOrange",
+            cssVariablePrefix: "secondary-orange",
+            shades: {
+                25: {
+                    hex: "#FFFAF5"
+                } as UntitledColorShadeInfo,
+                50: {
+                    hex: "#FFF6ED"
+                } as UntitledColorShadeInfo,
+                100: {
+                    hex: "#FFEAD5"
+                } as UntitledColorShadeInfo,
+                200: {
+                    hex: "#FDDCAB"
+                } as UntitledColorShadeInfo,
+                300: {
+                    hex: "#FEB273"
+                } as UntitledColorShadeInfo,
+                400: {
+                    hex: "#FD853A"
+                } as UntitledColorShadeInfo,
+                500: {
+                    hex: "#FB6514"
+                } as UntitledColorShadeInfo,
+                600: {
+                    hex: "#EC4A0A"
+                } as UntitledColorShadeInfo,
+                700: {
+                    hex: "#C4320A"
+                } as UntitledColorShadeInfo,
+                800: {
+                    hex: "#9C2A10"
+                } as UntitledColorShadeInfo,
+                900: {
+                    hex: "#7E2410"
                 } as UntitledColorShadeInfo,
             } as UntitledColorShadesList
         } as UntitledColorInfo;
