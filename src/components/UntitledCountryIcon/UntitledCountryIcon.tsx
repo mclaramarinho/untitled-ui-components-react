92e91c5ca40269ef0c0d0e508b18124cceecf9fd
mclaramarinho <marinho.claramb@gmail.com>
2025-02-12 21:48:55 -0300

    fixed country icon

--- src/components/UntitledCountryIcon/UntitledCountryIcon.tsx
+++ src/components/UntitledCountryIcon/UntitledCountryIcon.tsx
@@ -10,7 +10,7 @@ import { useImageProperties } from '../../hooks/useImageProperties';
 const UntitledCountryIcon: React.FC<UntitledCountryIconProps> = (props) => {
   const BASE_URL = "../assets/icons/countries/";
 
-  const { width, height, src } = useImageProperties({ assetBaseUrl: BASE_URL, assetPath: props.country, width: props.size?.width, height: props.size?.height})
+  const { width, height, src } = useImageProperties({ assetBaseUrl: BASE_URL, assetFileName: props.country, width: props.size?.width, height: props.size?.height})
 
   return <UntitledSvgRenderer img={src} width={width} height={height} altText={props.altText ?? `Icon ${props.country}`} />
 };
