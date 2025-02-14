a4c1decc93a26a0efc3b40856745a91e0d4fd82c
mclaramarinho <marinho.claramb@gmail.com>
2025-02-14 00:02:12 -0300

    updated scss directives

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import './index.scss';
 
-export * from './components';
+export {default as UntitledCountryIcon} from './components/UntitledCountryIcon';
+export {default as UntitledHeader} from './components/UntitledHeader';
+export {default as UntitledIcon} from './components/UntitledIcon';
+export {default as UntitledIntegrationIcon} from './components/UntitledIntegrationIcon';
+export {default as UntitledPaymentMethodIcon} from './components/UntitledPaymentMethodIcon';
+export {default as UntitledSocialIcon} from './components/UntitledSocialIcon';
+export {default as UntitledSvgRenderer} from './components/UntitledSvgRenderer';
